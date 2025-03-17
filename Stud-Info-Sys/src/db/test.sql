dc9182041eda731d17ce91e23594c7a44fca7205
Santo <santo2004off@gmail.com>
2025-03-17 21:42:49 +0530

    created a table in test db and also inserted sample records

--- Stud-Info-Sys/src/db/test.sql
+++ Stud-Info-Sys/src/db/test.sql
@@ -1,15 +1,22 @@
 -- Create the database
-CREATE DATABASE test;
+--CREATE DATABASE test;
 
 -- Use the created database
 USE test;
 
 -- Creating a student database
-create table stud(
+create table stds(
     name varchar(MAX),
     age INT, 
     class varchar(10)
-)
+);
+
+--inserting values into stud
+insert into stds(name,age,class) values
+    ('santo',21,'4th year'),
+    ('viji',20,'4th year'),
+    ('shruthi',20,'4th year'
+);
 
 --displaying the created table
-select * from stud
+select * from stds
