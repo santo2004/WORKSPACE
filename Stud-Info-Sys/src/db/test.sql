ab8de7ebb161a9a1d8a17da498ef1ba76be6ff5b
Santo <santo2004off@gmail.com>
2025-03-17 22:14:45 +0530

    added another column to the students table

--- Stud-Info-Sys/src/db/test.sql
+++ Stud-Info-Sys/src/db/test.sql
@@ -5,18 +5,26 @@
 USE test;
 
 -- Creating a student database
-create table stds(
+create table student(
     name varchar(MAX),
     age INT, 
     class varchar(10)
 );
 
 --inserting values into stud
-insert into stds(name,age,class) values
+insert into student(name,age,class) values
     ('santo',21,'4th year'),
     ('viji',20,'4th year'),
     ('shruthi',20,'4th year'
 );
 
+--adding a new column dob
+ALTER TABLE student ADD dob DATE;
+
+--updating values for the new columns
+UPDATE student SET dob = '2004-02-23' WHERE name = 'santo';
+UPDATE student SET dob = '2004-04-02' WHERE name = 'viji';
+UPDATE student SET dob = '2004-04-18' WHERE name = 'shruthi';
+
 --displaying the created table
-select * from stds
+SELECT * FROM student;
